6c20b639dd4c87856e448fd4f440c17c0d34625d
satoso <satoso@users.noreply.github.com>
2026-06-02 22:25:46 +0900

    xdg for vim

--- symlink.sh
+++ symlink.sh
@@ -3,6 +3,7 @@
 cd "$(dirname "$0")"
 ln -s `pwd`/.config/git ~/.config/git
 touch ~/.config/git/config
-ln -s `pwd`/.vim        ~/.vim
+# ln -s `pwd`/.config/vim ~/.vim  # for old vim
+ln -s `pwd`/.config/vim ~/.config/vim
 
 exit
